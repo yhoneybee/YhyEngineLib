408c173bb053136cfc7180d8a49242974496016a
yhoney <chaeyh04@gmail.com>
2023-07-10 00:21:58 +0900

    fix > 완전히 잘못 생각하고 있었음
    이 라이브러리에서 로그를 파일로 찍는 것이 아니라 flutter로 로그 내용을 전송해야함

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 
 #include "file.hpp"
-#include "log.hpp"
 #include "pipeline.hpp"
 #include "window.hpp"
 
@@ -11,18 +10,6 @@ using namespace std;
 constexpr auto nl = '\n';
 
 int main() {
-    yhy::Log::SetLogFolder(R"(E:\YhyEngineLogs)");
-
-    TRACE(main);
-
-    {
-        TRACE(Level1);
-        {
-            TRACE(Level2);
-            { TRACE(Level3); }
-        }
-    }
-
     yhy::Window window{ "first vulkan app", 800, 600 };
 
     while (window.Running()) {}
--- src/utils/file.cpp
+++ src/utils/file.cpp
@@ -8,7 +8,7 @@ BEGIN_NAMESPACE(yhy)
 
 File::Bytes File::Read(const std::filesystem::path& filePath) {
     std::ifstream reader{ filePath, std::ios::ate | std::ios::binary };
-    if (!reader) { Log::InsertLog("Failed to open file"); }
+    // if (!reader) { 유니티에서 처럼 에러를 flutter로 전송해야함 }
 
     auto file_size = static_cast<size_t>(reader.tellg());
     Bytes buffer;
@@ -23,7 +23,7 @@ File::Bytes File::Read(const std::filesystem::path& filePath) {
 
 void File::Write(const std::filesystem::path& filePath, std::string_view contents) {
     std::ofstream writer{ filePath, std::ios::app };
-    if (!writer) { Log::InsertLog("Failed to open file"); }
+    // if (!writer) { 유니티에서 처럼 에러를 flutter로 전송해야함 }
     writer << contents;
 }
 
